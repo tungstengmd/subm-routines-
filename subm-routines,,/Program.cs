fe5c7acd663da801d99c750d2de65aeeeb2ed060
Harlow Foxworthy <22BKarolia@heckgrammar.co.uk>
2025-09-17 14:51:59 +0100

    working code ?!

--- subm-routines,,/Program.cs
+++ subm-routines,,/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace subm_routines__
+﻿using System.Collections.Immutable;
+
+namespace subm_routines__
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] array = { 1, 2, 3, 4, 5 };
+            int[] array2 = new int[array.Length];
+            int value = 1;
+            for (int i = 0; i < array.Length; i++) {
+                array2 = array.Where((x,j) => j!=i).ToArray();
+                foreach (int k in array2) {
+                    value *= k;
+                }
+                Console.WriteLine(value);
+                value = 1;
+            }
         }
     }
 }
