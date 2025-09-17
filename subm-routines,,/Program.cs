d09d10392dd0d31372fcd355c5185ea28ee59f1c
Harlow Foxworthy <22BKarolia@heckgrammar.co.uk>
2025-09-17 15:12:03 +0100

    my coding style is cemented

--- subm-routines,,/Program.cs
+++ subm-routines,,/Program.cs
@@ -1,7 +1,5 @@
-﻿internal class Program
-{
-    static void Main(string[] args)
-    {
+﻿internal class Program {
+    static void Main() {
         Console.Write("enter a comma-separated list of numbers\n$ ");
         string[] arr = Console.ReadLine()!.Split(",");  
         int[] array = Array.ConvertAll(arr, int.Parse);
