c11cbfe5e0c394d385cb199432ef04ed8ffc542c
Harlow Foxworthy <22BKarolia@heckgrammar.co.uk>
2025-09-17 15:05:57 +0100

    woa

--- subm-routines,,/Program.cs
+++ subm-routines,,/Program.cs
@@ -1,22 +1,20 @@
-﻿using System.Collections.Immutable;
-
-namespace subm_routines__
+﻿internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            int[] array = { 1, 2, 3, 4, 5 };
-            int[] array2 = new int[array.Length];
-            int value = 1;
-            for (int i = 0; i < array.Length; i++) {
-                array2 = array.Where((x,j) => j!=i).ToArray();
-                foreach (int k in array2) {
-                    value *= k;
-                }
-                Console.WriteLine(value);
-                value = 1;
+        Console.Write("enter a comma-separated list of numbers\n$ ");
+        string[] arr = Console.ReadLine()!.Split(",");  
+        int[] array = Array.ConvertAll(arr, int.Parse);
+        int[] array2 = new int[array.Length];
+        int value = 1;
+        for (int i = 0; i < array.Length; i++) {
+            array2 = array.Where((x,j) => j!=i).ToArray();
+            foreach (int k in array2) {
+                value *= k;
             }
+            Console.WriteLine($"\x1b[92m{value}");
+            value = 1;
         }
+        Console.WriteLine("\x1b[0m");
     }
 }
