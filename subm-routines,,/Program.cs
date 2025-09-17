5c2536da7c2e47efe7f726809ccff84b76a36e3f
Harlow Foxworthy <22BKarolia@heckgrammar.co.uk>
2025-09-17 14:33:30 +0100

    Add project files.

---
+++ subm-routines,,/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace subm_routines__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
