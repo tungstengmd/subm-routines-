17ea408726f96af66a9d067315005776e4c0daf0
Aster Foxworthy <gostdud@gmail.com>
2025-09-18 18:04:02 +0100

    MORE CONDENSATION RAHHHHHHHHHH

--- subm-routines,,/Program.cs
+++ subm-routines,,/Program.cs
@@ -1,13 +1,10 @@
 ﻿internal class Program {
     static void Main() {
-        Console.Write("enter a comma-separated list of numbers\n$ ");
-        string[] arr = Console.ReadLine()!.Split(",");  
-        int[] array = Array.ConvertAll(arr, int.Parse);
-        int[] array2 = new int[array.Length];
+        Console.Write("enter a comma-separated list of numbers\n% ");
+        int[] array = Array.ConvertAll(Console.ReadLine()!.Split(","), int.Parse);
         int value = 1;
         for (int i = 0; i < array.Length; i++) {
-            array2 = array.Where((x,j) => j!=i).ToArray();
-            foreach (int k in array2) {
+            foreach (int k in array.Where((x,j) => j!=i).ToArray()) {
                 value *= k;
             }
             Console.WriteLine($"\x1b[92m{value}");
